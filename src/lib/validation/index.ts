2f83d8d417283f10cee41262997abc652e4ff6df
Sharkty21 <tylermsharkey@gmail.com>
2023-12-21 09:54:00 -0600

    sign in and sign up done

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,13 @@
 import * as z from "zod"
 
 export const SignupValidation = z.object({
-    username: z.string().min(2).max(50),
+    name: z.string().min(2, { message: 'Too short' }),
+    username: z.string().min(2, { message: 'Too short'}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Too short'})
+  })
+
+  export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Too short'})
   })
